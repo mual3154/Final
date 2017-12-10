8bb70b821a2b89d4d996a475f86835975ae9ac24
Muntadher AlZayer <mual3154@colorado.com>
2017-12-10 16:29:53 -0700

    finished prob 4?

--- graphExample.py
+++ graphExample.py
@@ -5,11 +5,63 @@ Created on Sat Nov 11 15:29:04 2017
 
 @author: rhoenigman
 """
-
+from collections import defaultdict
 import networkx as nx
-
+from collections import deque
 #create a directed graph
 G = nx.DiGraph()
+
+def bfs(self, current, dest, pathamount):
+	total = 0
+	arrows = [ ]
+	arrows = self.successors(current)
+	que = [ ]
+	for x in arrows:
+		que.append(x)
+	q = 0
+	for q in range(len(que)):
+		if que[q] == dest:
+			total = total + 1
+			print "added 1 to total for it to be ", total
+		else :
+			print "called bfs. ", current , " -> " , que[q]
+			total = total + bfs(self, que[q], dest, total)
+	return total
+	
+
+def printAllPathsUtil(self, u, d, visited, path):
+ 
+        # Mark the current node as visited and store in path
+        visited[u]= True
+        path.append(u)
+ 
+        # If current vertex is same as destination, then print
+        # current path[]
+        if u ==d:
+            print path
+        else:
+            # If current vertex is not destination
+            #Recur for all the vertices adjacent to this vertex
+            for i in self.graph[u]:
+                if visited[i]==False:
+                    self.printAllPathsUtil(i, d, visited, path)
+                     
+        # Remove current vertex from path[] and mark it as unvisited
+        path.pop()
+        visited[u]= False
+  
+  
+# Prints all paths from 's' to 'd'
+def printAllPaths(self,s, d):
+ 
+        # Mark all the vertices as not visited
+        visited =[False]*(self.V)
+ 
+        # Create an array to store paths
+        path = []
+ 
+        # Call the recursive helper function to print all paths
+        self.printAllPathsUtil(s, d,visited, path)
 
 #adding an edge also adds the node
 G.add_edge('Spider', 'A', weight=1.0)
@@ -72,8 +124,12 @@ G.add_edge('L', 'T', weight=1.0)
 #each edge has a weight of 1. The shortest path is the fewest edges.
 #Use this to verify that your graph built correctly.
 t = nx.shortest_path(G, 'Spider', 'Fly', weight='weight')
+print t
+t = []
+t = G.successors('A')
+for c in t:
+	print c
 
-print(t)
-
-
-
+totalpaths = 0
+m = bfs(G, 'Spider', 'Fly', totalpaths)
+print(m)
